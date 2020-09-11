451b831c2f2bdbf890d2b732ef494927991b1fad
nelsonlai1 <96a4a08@gmail.com>
2020-09-11 17:56:27 +0800

    Fix former; Add reverse

--- queue.c
+++ queue.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 //#include "harness.h"
-#include "assert.h"
 #include "queue.h"
 #ifndef strlcpy
 #define strlcpy(dst, src, sz) snprintf((dst), (sz), "%s", (src))
@@ -15,7 +14,8 @@ queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
     /* TODO: What if malloc returned NULL? */
-    assert(q);
+    if (q == NULL)
+        return NULL;
     q->head = q->tail = NULL;
     q->size = 0;
     return q;
@@ -26,9 +26,9 @@ void q_free(queue_t *q)
 {
     /* TODO: How about freeing the list elements and the strings? */
     /* Free queue structure */
-    if (!q)
+    if (q == NULL)
         return;
-    while (q->head) {
+    while (q->head != NULL) {
         list_ele_t *tmp = q->head;
         q->head = q->head->next;
         free(tmp);
@@ -45,19 +45,27 @@ void q_free(queue_t *q)
  */
 bool q_insert_head(queue_t *q, char *s)
 {
+    if (q == NULL)
+        return false;
     list_ele_t *newh;
     /* TODO: What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL)
+        return false;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
-    assert(newh);
     newh->value = malloc(sizeof(char) * (strlen(s) + 1));
-    assert(newh->value);
+    if (newh->value == NULL) {
+        free(newh);
+        return false;
+    }
     strlcpy(newh->value, s, strlen(s) + 1);
-    if (q->head)
+    if (q->head != NULL)
         newh->next = q->head;
-    else
+    else {
         q->tail = newh;
+        newh->next = NULL;
+    }
     q->head = newh;
     q->size++;
     return true;
@@ -72,18 +80,24 @@ bool q_insert_head(queue_t *q, char *s)
  */
 bool q_insert_tail(queue_t *q, char *s)
 {
+    if (q == NULL)
+        return false;
     list_ele_t *newt;
     newt = malloc(sizeof(list_ele_t));
-    assert(newt);
+    if (newt == NULL)
+        return false;
     newt->value = malloc(sizeof(char) * (strlen(s) + 1));
-    assert(newt->value);
+    if (newt->value == NULL) {
+        free(newt);
+        return false;
+    }
     strlcpy(newt->value, s, strlen(s) + 1);
-    newt->next = NULL;
-    if (q->tail)
+    if (q->tail != NULL)
         q->tail->next = newt;
     else
         q->head = newt;
     q->tail = newt;
+    newt->next = NULL;
     q->size++;
     return true;
 }
@@ -99,10 +113,21 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
-    if (!q || !q->head)
+    if (q == NULL || q->head == NULL || sp == NULL)
         return false;
-    if (sp)
-        q->head = q->head->next;
+    memset(sp, '\0', bufsize);
+    if (bufsize > strlen(q->head->value))
+        strlcpy(sp, q->head->value, strlen(q->head->value) + 1);
+    else
+        strlcpy(sp, q->head->value, bufsize);
+    list_ele_t *rm = q->head;
+    q->head = q->head->next;
+    rm->next = NULL;
+    free(rm->value);
+    free(rm);
+    q->size--;
+    if (q->size == 0)
+        q->tail = NULL;
     return true;
 }
 
@@ -112,7 +137,7 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    if (!q)
+    if (q == NULL)
         return 0;
     return q->size;
 }
@@ -128,6 +153,19 @@ void q_reverse(queue_t *q)
 {
     /* TODO: You need to write the code for this function */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (q == NULL || q->head == NULL)
+        return;
+    list_ele_t *cursor = NULL;
+    q->tail = q->head;
+    while (q->head != NULL && q->head->next != NULL) {
+        list_ele_t *next = q->head->next;
+        if (cursor == NULL)
+            q->head->next = NULL;
+        else
+            q->head->next = cursor;
+        cursor = q->head;
+        q->head = next;
+    }
 }
 
 /*
